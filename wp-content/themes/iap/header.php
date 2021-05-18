3868f3058d20dffe13a0f8d7354c7ab1b84ab8f0
Tiberiu Rus <ro.tiberiu@gmail.com>
2021-05-18 20:12:14 +0300

    WIP header + footer

--- wp-content/themes/iap/header.php
+++ wp-content/themes/iap/header.php
@@ -8,3 +8,16 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+
+<header class="header">
+	<div class="container">
+		<a href="/"><img src="<?= get_stylesheet_directory_uri() . "/images/logo.svg" ?>" /></a>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Find a doctor</a></li>
+			<li><a href="#">Apps</a></li>
+			<li><a href="#">Testimonials</a></li>
+			<li><a href="#">About us</a></li>
+		</ul>
+	</div>
+</header>
--- wp-content/themes/iap/index.php
+++ wp-content/themes/iap/index.php
@@ -1,5 +1,7 @@
 <?php get_header() ?>
 
-Homepage
+Zona cu poza si text
+
+Zona cu testimoniale
 
 <?php get_footer() ?>
