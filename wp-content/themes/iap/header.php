44511c32a3ca5a9f3caa9b5eec4f5ce082ccd095
Tiberiu Rus <ro.tiberiu@gmail.com>
2021-06-10 21:13:25 +0300

    WIP homepage; added flexbox grid

--- wp-content/themes/iap/header.php
+++ wp-content/themes/iap/header.php
@@ -9,15 +9,17 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-<header class="header">
-	<div class="container">
-		<a href="/"><img src="<?= get_stylesheet_directory_uri() . "/images/logo.svg" ?>" /></a>
-		<ul>
-			<li><a href="#">Home</a></li>
-			<li><a href="#">Find a doctor</a></li>
-			<li><a href="#">Apps</a></li>
-			<li><a href="#">Testimonials</a></li>
-			<li><a href="#">About us</a></li>
-		</ul>
-	</div>
-</header>
+	<header class="header">
+		<div class="container">
+			<a href="/"><img src="<?= get_stylesheet_directory_uri() . "/images/logo.svg" ?>" /></a>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Find a doctor</a></li>
+				<li><a href="#">Apps</a></li>
+				<li><a href="#">Testimonials</a></li>
+				<li><a href="#">About us</a></li>
+			</ul>
+		</div>
+	</header>
+
+	
--- wp-content/themes/iap/index.php
+++ wp-content/themes/iap/index.php
@@ -1,7 +1,9 @@
 <?php get_header() ?>
 
-Zona cu poza si text
-
-Zona cu testimoniale
+<?php get_template_part('template-parts/homepage/hero-top'); ?>
+<?php get_template_part('template-parts/homepage/services'); ?>
+<?php get_template_part('template-parts/homepage/hero-leading'); ?>
+<?php get_template_part('template-parts/homepage/hero-download'); ?>
+<?php get_template_part('template-parts/homepage/blog'); ?>
 
 <?php get_footer() ?>
